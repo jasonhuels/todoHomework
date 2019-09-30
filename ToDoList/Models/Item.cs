636457e70a055222e966333e017f0103db64b8c0
Christopher Burge and Ethan Samuels-Ellingson <ethan@>
2019-09-30 15:58:58 -0700

    add first test and minimal code to pass test.

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,19 @@
+namespace ToDoList.Models
+{
+  public class Item
+  {
+        public string Description { get; set; }
+        private static List<Item> _instances = new List<Item> {};
+
+        public Item(string description)
+        {
+            Description = description;
+            _instances.Add(this);
+        }
+
+        public static List<Item> GetAll()
+        {
+            return _instances;
+        }
+  }
+}

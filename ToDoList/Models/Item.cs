5d44decf4b8986f836fac8a446d02b677a62bc80
Student <example@example.com>
2019-10-07 16:32:03 -0700

    add mvc scaffolding.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,35 +1,27 @@
-using System;
 using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
   public class Item
   {
-        public string Description { get; set; }
-        private static List<Item> _instances = new List<Item> {};
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
 
-        public Item(string description)
-        {
-            Description = description;
-            _instances.Add(this);
-        }
+    public Item (string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
 
-        public static List<Item> GetAll()
-        {
-            return _instances;
-        }
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
 
-        public static void ClearAll()
-        {
-            _instances.Clear();
-        }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
 
-      class Program 
-      {
-           static void Main ()
-           {
-
-           }
-      }  
   }
 }
